88d8384a21229ba6032da1e8ed0620d2df837b0e
Samar <33544311+god-s-perfect-idiot@users.noreply.github.com>
2020-08-31 15:42:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, render_template_string, send_from_directory, abort, safe_join, send_file 
+from flask import Flask, render_template, request, render_template_string, send_from_directory, abort, safe_join, send_file
 from nocache import nocache
 import model
 import service
 import os
+import json
 
 prev=''
 app = Flask(__name__)
@@ -12,7 +13,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/fetch", methods=["POST"])
+def fetch():
+    if(request.method == 'POST'):
+        term = request.form.get('searchterm')
 
+        return json.jsonify("worked")
 
 if(__name__=="__main__"):
     app.run(debug=True)
